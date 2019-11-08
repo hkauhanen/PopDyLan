b93181ff6e383110751fc5309704a42e6edfa82a
Henri Kauhanen <nenahuak@gmail.com>
2019-11-08 16:53:31 +0100

    work on ling.jl plus docs

--- src/PopDyLan.jl
+++ src/PopDyLan.jl
@@ -6,4 +6,10 @@ module PopDyLan
 include("ling.jl") # general linguistics types and methods
 include("VL.jl") # variational learning
 
+export Variable
+export Variable(n::Int)
+export Variable(p::Array)
+export VL
+export listen!
+
 end
